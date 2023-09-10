30feb55a86416f0849dfa6ffa681be92f9443da5
eddiebee <edbeeotudor@gmail.com>
2023-09-10 05:41:49 +0100

    test out a simple file download functionality

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/download')
+def download_file():
+    return send_file('hf-logo.png', as_attachment=True)
